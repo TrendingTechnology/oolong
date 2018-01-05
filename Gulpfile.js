8fab518340a9b97b75d96b027bc693d09e97b74e
CodeHat <dev@codehat.de>
2018-01-05 20:50:43 +0100

    Update navbar, Fix pagination, Move Facebook and Twitter icons

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,11 @@
 let gulp = require('gulp');
 let sass = require('gulp-sass');
 let cleanCSS = require('gulp-clean-css');
+let minifyJS = require('gulp-js-minify');
 
 // Compile SASS task
 gulp.task('styles', () => {
-    gulp.src('assets/sass/*.scss')
+    gulp.src('src/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('assets/css/'))
 });
@@ -16,9 +17,16 @@ gulp.task('minify-css', () => {
       .pipe(gulp.dest('assets/css'));
 });
 
+// Minify JS task
+gulp.task('minify-js', function(){
+    gulp.src('src/js/*.js')
+      .pipe(minifyJS())
+      .pipe(gulp.dest('assets/js'));
+  });
+
 // Watch task
 gulp.task('default', () => {
-    gulp.watch('assets/sass/*.scss', ['styles']);
+    gulp.watch('src/sass/*.scss', ['styles']);
 });
 
 // Development task
@@ -33,4 +41,5 @@ gulp.task('prod', () => {
     console.log('-- Production --')
     gulp.start('styles');
     gulp.start('minify-css');
+    gulp.start('minify-js');
 });
--- assets/js/oolong.js
+++ assets/js/oolong.js
@@ -1,11 +1,1 @@
-$(document).ready(function () {
-    console.log("Ready!")
-
-    $('.navbar-burger').each(function (index, element) {
-       $(this).on('click', function () {
-           var target = $(this).data('target');
-           $(element).toggleClass('is-active');
-           $('#' + target).toggleClass('is-active');
-       })
-    });
-});
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--)d[c.toString(a)]=k[c]||c.toString(a);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('3.9(\'f\',5(){6 $4=g.o.n.m(3.l(\'.k-j\'),0);i($4.h>0){$4.p(5($2){$2.9(\'c\',5(){6 1=$2.e.1;6 $1=3.d(1);$2.8.7(\'a-b\');$1.8.7(\'a-b\')})})}});',26,26,'|target|el|document|navbarBurgers|function|var|toggle|classList|addEventListener|is|active|click|getElementById|dataset|DOMContentLoaded|Array|length|if|burger|navbar|querySelectorAll|call|slice|prototype|forEach'.split('|'),0,{}))
